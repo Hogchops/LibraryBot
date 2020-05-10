8ce64e9c631ae7dfe450b6bcb1aa4fb0d7082958
Hogchops <hogchops@gmail.com>
2020-05-10 00:23:42 -0400

    Create UserNode.cs

---
+++ src/UserNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryBot
+{
+    public class UserNode
+    {
+        private IUser usr;
+        private int init;
+
+        public UserNode(IUser usr)
+        {
+            this.usr = usr;
+        }
+
+        public void setInit(int init)
+        {
+            this.init = init;
+        }
+
+        public IUser getUser()
+        {
+            return usr;
+        }
+
+        public int getInit()
+        {
+            return init;
+        }
+    }
+}
